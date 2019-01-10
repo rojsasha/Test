11f63f4bc43d3f91a1f2cadc94a6d6a2ffa02d27
alex <rojsasha@gmail.com>
2019-01-10 20:36:06 +0600

    test

---
+++ app/src/main/java/com/example/alex/test/EntryActivity.java
@@ -0,0 +1,40 @@
+package com.example.alex.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class EntryActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+        showAuth();
+    }
+
+    private void showAuth() {
+        switchFragment(new AuthFragment());
+    }
+
+    private void switchFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.entry_container, fragment)
+                .commit();
+    }
+
+    private void showMain() {
+        switchFragment(new MainFragment());
+    }
+
+    public static class AuthFragment extends Fragment {
+
+        public final void onAuthSuccess() {
+            EntryActivity activity = (EntryActivity) getActivity();
+            activity.switchFragment(new MainFragment());
+        }
+    }
+
+    public static class MainFragment extends Fragment {
+    }
+}
